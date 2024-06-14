af1f42bef4b75c23f2957ba4e6027127d2f716bc
BartkowskyPjatk <163758100+BartkowskyPjatk@users.noreply.github.com>
2024-06-15 01:41:13 +0200

    css dodany + skrypty

---
+++ script.js
@@ -0,0 +1,37 @@
+function click(){
+    console.log("click")
+}
+const guzikGora = document.getElementById("contactRollDown");
+const guzikDol = document.getElementById("contactRollUp");
+const checkbox = document.getElementById('menuTap');
+const scrollingElement = (document.scrollingElement || document.body);
+
+const scrollSmoothToBottom = () => {
+    console.log("clickDown");
+    $(scrollingElement).animate({
+        scrollTop: document.body.scrollHeight
+    }, 500);
+};
+const scrollSmoothToTop = () => {
+    console.log("clickUp");
+    $(scrollingElement).animate({
+        scrollTop: 0
+    }, 500);
+};
+guzikGora.addEventListener("click", scrollSmoothToBottom);
+guzikDol.addEventListener("click", scrollSmoothToTop);
+
+    checkbox.addEventListener('change',function (){
+        if(this.checked){
+            $(scrollingElement).animate({
+                scrollLeft: document.body.scrollWidth,
+                scrollTop: 0
+            }, 500);
+        }
+        else {
+            $(scrollingElement).animate({
+                scrollLeft: 0,
+                scrollTop: 0
+            }, 500);
+        }
+    })
